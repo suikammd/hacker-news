00d586910823d28b443b936ba644e10a45e5c383
王宇华 <suikalala@gmail.com>
2019-02-19 11:04:38 +0800

    finish db and api provider

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,60 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async{
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, "items.db");
+
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE Items
+            (
+              id INTEGER PRIMARY KEY,
+              type TEXT,
+              by TEXT,
+              time INTEGER,
+              text TEXT,
+              parent INTEGER,
+              kid BLOB,
+              dead INTEGER,
+              deleted INTEGER,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendent INTEGER
+            )
+        """);
+      }
+    );
+  }
+
+
+  fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0 ) {
+      ItemModel.fromDb(maps.first)
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel item) {
+    return db.insert("Items", item.toMapForDb());
+  }
+}
